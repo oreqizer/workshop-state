fcbe3bba9810459c40dd8010c71c1f070b4c0bae
oreqizer <oreqizer@gmail.com>
2017-09-22 14:47:57 +0200

    render: Add store

--- src/render.jsx
+++ src/render.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { combineEpics, createEpicMiddleware } from 'redux-observable';
+import { createStore, applyMiddleware } from 'redux';
 import './render.css';
 import Root from './scenes/Root';
+import todosReducer from './scenes/Todo/services';
+import todosEpic from './scenes/Todo/services/epics';
 import registerServiceWorker from './browser/registerServiceWorker';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const epic = combineEpics(
+  todosEpic,
+);
+
+const epicMw = createEpicMiddleware(epic);
+
+const store = createStore({
+  todos: todosReducer,
+}, applyMiddleware(epicMw));
+
+ReactDOM.render((
+  <Provider store={store}>
+    <Root />
+  </Provider>
+), document.getElementById('root'));
 registerServiceWorker();
