ae7f5a1df706c80b2e42b1c634ada6d82831570a
oreqizer <oreqizer@gmail.com>
2017-09-22 14:33:39 +0200

    Todos: Add things

--- src/scenes/Todo/services/epics.js
+++ src/scenes/Todo/services/epics.js
@@ -1,2 +1,77 @@
 /* @flow */
-// business logic + side effects
+import 'rxjs';
+import { combineEpics } from 'redux-observable';
+
+import * as actions from './actions';
+import * as api from './api';
+
+const getTodosEpic = action$ =>
+  action$
+    .filter(action => action.type === actions.GET)
+    .flatMap(() => (
+      api
+        .getTodos()
+        .then(data => ({
+          type: actions.GET_SUCCESS,
+          payload: { data },
+        }))
+        .catch(error => ({
+          type: actions.GET_ERROR,
+          payload: { error },
+        }))
+    ));
+
+const createTodoEpic = action$ =>
+  action$
+    .filter(action => action.type === actions.CREATE)
+    .flatMap(action => (
+      api
+        .postTodo(action.payload.text)
+        .then(id => ({
+          type: actions.CREATE_SUCCESS,
+          payload: { id, text: action.payload.text },
+        }))
+        .catch(error => ({
+          type: actions.CREATE_ERROR,
+          payload: { error },
+        }))
+    ));
+
+const editTodoEpic = action$ =>
+  action$
+    .filter(action => action.type === actions.EDIT)
+    .flatMap(action => (
+      api
+        .putTodo(action.payload.id, action.payload.text)
+        .then(() => ({
+          type: actions.EDIT_SUCCESS,
+          payload: action.payload,
+        }))
+        .catch(error => ({
+          type: actions.EDIT_ERROR,
+          payload: { error },
+        }))
+    ));
+
+const deleteTodoEpic = action$ =>
+  action$
+    .filter(action => action.type === actions.DELETE)
+    .flatMap(action => (
+      api
+        .deleteTodo(action.payload.id)
+        .then(() => ({
+          type: actions.DELETE_SUCCESS,
+          payload: { id: action.payload.id },
+        }))
+        .catch(error => ({
+          type: actions.DELETE_ERROR,
+          payload: { error },
+        }))
+    ));
+
+export default combineEpics(
+  getTodosEpic,
+  createTodoEpic,
+  editTodoEpic,
+  deleteTodoEpic,
+);
--- src/scenes/Todo/services/index.js
+++ src/scenes/Todo/services/index.js
@@ -1,2 +1,65 @@
 /* @flow */
-// reducer + selectors
+import R from 'ramda';
+
+import type { Todo } from '../../../data/Todo';
+import type { Action } from './actions';
+import * as actions from './actions';
+
+export type TodoState = {
+  loading: boolean,
+  todos: Todo[],
+};
+
+const initialState = {
+  loading: false,
+  todos: [],
+};
+
+function todoReducer(state: TodoState = initialState, action: Action) {
+  switch (action.type) {
+    case actions.GET:
+    case actions.CREATE:
+    case actions.EDIT:
+    case actions.DELETE:
+      return R.assoc('loading', true, state);
+
+    case actions.GET_SUCCESS:
+      return R.compose(
+        R.assoc('todos', action.payload.data),
+        R.assoc('loading', false),
+      )(state);
+
+    case actions.CREATE_SUCCESS:
+      return R.compose(
+        R.over(R.lensProp('todos'), R.append(action.payload)),
+        R.assoc('loading', false),
+      )(state);
+
+    case actions.EDIT_SUCCESS:
+      return R.compose(
+        R.over(R.lensProp('todos'), R.map((todo) => {
+          if (todo.id === action.payload.id) {
+            return action.payload;
+          }
+
+          return todo;
+        })),
+        R.assoc('loading', false),
+      )(state);
+
+    case actions.DELETE_SUCCESS:
+      return R.compose(
+        R.over(R.lensProp('todos'), R.filter(todo => todo.id !== action.payload.id)),
+        R.assoc('loading', false),
+      )(state);
+
+    default:
+      return state;
+  }
+}
+
+export default todoReducer;
+
+const getState = state => state.todos;
+
+// TODO other selectors
